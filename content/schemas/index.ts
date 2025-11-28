14ab6fc1770819044394707302874ee152063be8
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-28 00:09:24 -0500

    removing some schemas from sanity ui - but will keep it in the code for now - over

--- content/schemas/index.ts
+++ content/schemas/index.ts
@@ -9,4 +9,4 @@ import home from "./home";
 import header from "./home/header";
 import about from "./home/about";
 // l'ordre de ce tableau va definir l'ordre dans la liste dans l'affichage
-export const schemaTypes = [home, about, header, homeGrid, posts, author, shop, blockContent];
+export const schemaTypes = [posts, author, shop, blockContent];
