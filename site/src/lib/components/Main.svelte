7c769182ba3e250dff0909e07347341919d71ea1
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-29 19:35:50 -0500

    code is good, but will refactor this later
    - i'm not sure if i want this component to work this way
    - i'll decide later
    - over

--- site/src/lib/components/Main.svelte
+++ site/src/lib/components/Main.svelte
@@ -1,18 +1,27 @@
 <script>
-    import PostList from "./PostList.svelte";
-    import Pagination from "./Pagination.svelte";
-    import { posts } from "../store/posts";
+	import PostList from "./PostList.svelte";
+	import Pagination from "./Pagination.svelte";
+	import { useFetchPosts } from "../helpers/useFetchPosts";
 
-    console.log($posts);
+	// import { posts } from "../store/posts";
 
-    let currentPage = 1;
-    let postsPerPage = 8;
+	const { fetchPosts } = useFetchPosts();
+	const post_promesse = fetchPosts();
 
-    $: indexOfLastPost = currentPage * postsPerPage;
-    $: indexOfFirstPost = indexOfLastPost - postsPerPage;
-    $: currentPosts = $posts.slice(indexOfFirstPost, indexOfLastPost);
+	let currentPage = $state(1);
+	let postsPerPage = $state(8);
+
+	let indexOfLastPost = $derived(currentPage * postsPerPage);
+	let indexOfFirstPost = $derived(indexOfLastPost - postsPerPage);
+	// let currentPosts = $derived($posts.slice(indexOfFirstPost, indexOfLastPost));
 </script>
 
 <main>
-    <PostList posts={$posts} />
+	{#await post_promesse}
+		<h2>Fetching posts...</h2>
+	{:then posts}
+		<PostList {posts} />
+	{:catch}
+		<h4>Trouble retrieving posts</h4>
+	{/await}
 </main>
