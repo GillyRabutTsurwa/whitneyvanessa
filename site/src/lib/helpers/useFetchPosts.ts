e8b9156f154267cbe40f932481958b36c4683fb3
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-29 19:30:39 -0500

    gradually refining sanity queries

--- site/src/lib/helpers/useFetchPosts.ts
+++ site/src/lib/helpers/useFetchPosts.ts
@@ -3,7 +3,24 @@ import { client } from "../../sanity.client";
 
 export const useFetchPosts = () => {
 	async function fetchPosts() {
-		const query = groq`*[_type == "post"]`;
+		const query = groq`*[_type == "post"] {
+			_id,
+			title,
+			slug,
+			author->,
+			postGenre,
+			categories,
+			thumbnail,
+			mainImage,
+			publishedAt,
+			_updatedAt,
+			// "slug": slug.current,
+			excerpt,
+			body,
+			colourPrimary,
+            colourSecondary,
+			_type
+    	}`;
 		const posts = await client.fetch(query);
 		return posts;
 	}
@@ -13,6 +30,29 @@ export const useFetchPosts = () => {
 		return posts.filter((currentPost: object) => currentPost.categories.includes(category));
 	}
 
+	async function fetchPost(url: string) {
+		const query = groq`*[_type == "post" && slug.current == "${url}"][0] {
+        _id,
+        title,
+        slug,
+        categories,
+        author->,
+        postGenre,
+        thumbnail,
+        mainImage,
+        publishedAt,
+        _updatedAt,
+        // "slug": slug.current,
+        excerpt,
+        body,
+        colourPrimary,
+            colourSecondary,
+        _type
+    }`;
+		const post = await client.fetch(query);
+		return post;
+	}
+
 	async function fetchRandomPost() {
 		const posts = await fetchPosts();
 		return posts[Math.floor(Math.random() * posts.length)];
@@ -20,6 +60,7 @@ export const useFetchPosts = () => {
 
 	return {
 		fetchPosts: fetchPosts,
+		fetchPost: fetchPost,
 		filterPosts: filterPosts,
 		fetchRandomPost: fetchRandomPost
 	};
