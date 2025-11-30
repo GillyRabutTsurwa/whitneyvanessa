043f23488a778aa4c787265dfcd885c5cf2ed6f3
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-29 19:29:02 -0500

    the parent component now fetches the featured post and passes it down
    - making use of props so that children components do less work
    - over

--- site/src/lib/components/FeaturedPost.svelte
+++ site/src/lib/components/FeaturedPost.svelte
@@ -1,37 +1,28 @@
 <script lang="ts">
 	import Button from "./Button.svelte";
-	import { useFetchPosts } from "../helpers/useFetchPosts";
 	import { useSanityImage } from "../helpers/useSanityImage";
+	let { post } = $props();
 
-	const { fetchRandomPost } = useFetchPosts();
+	console.log(post);
+
 	const { urlFor } = useSanityImage();
-	const postPromesse = fetchRandomPost();
-	console.log(postPromesse);
 
-	let snippetLength = 300;
-	// move this. je l'utilise d'ailleurs
-	function getSnippet(blockContent) {
-		const body = blockContent
-			.filter((block) => block._type === "block")
-			.map((block) => block.children.map((child) => child.text).join(""))
-			.join("");
-		return body.slice(0, snippetLength) + "...";
+	let snippetLength = 300; //NOTE: me faut pas l'écrire ainsi $state car ce ne sera pas réactif
+
+	function getSnippet(text: string) {
+		return text.slice(0, snippetLength) + "...";
 	}
 </script>
 
-<h2 style="text-align: center;">Featured Post</h2>
-{#await postPromesse}
-	<h5>Fetching Featured Post</h5>
-{:then post}
-	<section>
-		<figure>
-			<img src={urlFor(post.mainImage)} alt="" />
-		</figure>
-		<h3>{post.title}</h3>
-		<p>{getSnippet(post.body)}</p>
-		<Button isLink path={`/posts/${post.slug.current}`} colourPrimary="#171717" colourSecondary="#dedede" />
-	</section>
-{/await}
+<section>
+	<h2 style="text-align: center;">Featured Post</h2>
+	<figure>
+		<img src={urlFor(post.mainImage)} alt="" />
+	</figure>
+	<h3>{post.title}</h3>
+	<p>{getSnippet(post.excerpt)}</p>
+	<Button isLink path={`/posts/${post.slug.current}`} colourPrimary="#171717" colourSecondary="#dedede" />
+</section>
 
 <style lang="scss">
 	section {
