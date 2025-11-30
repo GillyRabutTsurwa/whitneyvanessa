f10fe76aeb09fdcf1d00480c7e4e04a93c5c9891
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-29 19:30:17 -0500

    making the dimensions of the logo dynamic
    - this works but i did it better in my site
    - i'll be sure to refactor this later
    - but i like it. it works well
    - over

--- site/src/lib/components/Logo.svelte
+++ site/src/lib/components/Logo.svelte
@@ -1,23 +1,24 @@
 <script>
-    import img from "../assets/img/logo.svg";
+	import img from "@/lib/assets/img/logo.svg";
+	let { size = 8 } = $props();
 </script>
 
-<figure>
-    <a href="/">
-        <img src={img} alt="" />
-    </a>
+<figure style:width={`${size}rem`} style:height={`${size}rem`}>
+	<a href="/" aria-label="go-home">
+		<img src={img} alt="logo" />
+	</a>
 </figure>
 
 <style lang="scss">
-    figure {
-        display: inline-block;
-        width: 8rem;
-        height: 8rem;
-        z-index: 1000;
+	figure {
+		display: inline-block;
+		// width: 8rem;
+		// height: 8rem;
+		z-index: 1000;
 
-        img {
-            width: 100%;
-            height: 100%;
-        }
-    }
+		img {
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
